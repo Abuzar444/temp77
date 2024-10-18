ac0922380b1d376eed7c864741f0d004b64e4642
abuzar <abuzardawar444@gmail.com>
2024-10-18 03:36:31 -0700

    commit

--- components/booking/BookingCalender.tsx
+++ components/booking/BookingCalender.tsx
@@ -29,7 +29,7 @@ function BookingCalendar() {
 
     useEffect(() => {
         const selectedRage = generateDateRange(range)
-        selectedRage.some((date) => {
+        const isDisabledDateIncluded = selectedRage.some((date) => {
             if (unavailablesDates[date]) {
                 setRange(defaultSelected)
                 toast({
